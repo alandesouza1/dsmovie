c073a991cb7dcb9025c4dcd14d0211f4232b42ff
Alan <socyware@gmail.com>
2022-01-10 23:53:50 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+function Form(){
+
+    return(
+        <h1>Page Forms </h1>
+
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+function Listing(){
+
+    return(
+        <h1>Page Listing </h1>
+
+    );
+}
+
+export default Listing;
